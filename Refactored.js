1a7ee815f53caeb269e1a6748032b227c1ef974b
TriggerSilva <canalampmoficial@gmail.com>
2020-02-11 10:52:01 +0000

    commit

--- Refactored.js
+++ Refactored.js
@@ -1,1 +1,17 @@
 //TEST 
+
+var numOne = 5;
+var numTwo = 10;
+
+
+const isLesserThan = function(numOne,numTwo)
+{
+    if (numOne > numTwo)
+    {
+
+        return true;
+    }else
+    {
+        return false;
+    }
+}
